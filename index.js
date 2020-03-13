7487f57b3f595294ccd355f601049777579bfe49
Ian Philips <ianphilips@Ians-MacBook-Pro.local>
2020-03-13 15:13:35 -0600

    app.tsx is for native

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import './shim.js'
 import crypto from 'crypto'
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import App from './src/native/App';
 import {name as appName} from './app.json';
 import React from "react";
 require('node-libs-react-native/globals');
 AppRegistry.registerComponent(appName, ()=>App);
 
-require('./src/App.tsx');
+require('./src/native/App.js');
